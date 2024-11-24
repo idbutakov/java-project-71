ef05149d933bcebc337cb85b1b8387bcc96d0d4b
i.butakov <i.butakov@tinkoffinsurance.ru>
2024-11-24 18:52:09 +0500

    push4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.*;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -10,15 +11,23 @@ import picocli.CommandLine.Option;
         description = "Compares two configuration files and shows a difference."
 )
 public class App implements Runnable{
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    private boolean help;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    private boolean version;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    private String format;
 
     @Override
     public void run() {
-        System.out.println("Run the diff logic here.");
+        System.out.printf("Comparing files '%s' and '%s' with format '%s'%n", filepath1, filepath2, format);
     }
 
     public static void main(String[] args) {
