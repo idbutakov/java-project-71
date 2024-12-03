96d71dc7c45b30a117c0a31178378d0d447aafe5
i.butakov <i.butakov@tinkoffinsurance.ru>
2024-12-03 18:33:48 +0500

    push45

--- app/src/test/java/ParserTests.java
+++ app/src/test/java/ParserTests.java
@@ -8,10 +8,11 @@ import java.io.IOException;
 import java.util.Map;
 
 class ParserTests {
+    private static final int TIMEOUT_1 = 50;
+    private static final int TIMEOUT_2 = 20;
 
     @Test
     void testParseFileValidJsonFile() throws IOException {
-        int timeout = 50;
         String filePath = "src/test/resources/json1.json";
         Map<String, Object> result = Parser.parseFile(filePath);
 
@@ -21,14 +22,13 @@ class ParserTests {
         assertTrue(result.containsKey("proxy"));
         assertTrue(result.containsKey("follow"));
         assertEquals("hexlet.io", result.get("host"));
-        assertEquals(timeout, result.get("timeout"));
+        assertEquals(TIMEOUT_1, result.get("timeout"));
         assertEquals("123.234.53.22", result.get("proxy"));
         assertEquals(false, result.get("follow"));
     }
 
     @Test
     void testParseFileValidYamlFile() throws IOException {
-        int timeout = 50;
         String filePath = "src/test/resources/yaml1.yaml";
         Map<String, Object> result = Parser.parseFile(filePath);
 
@@ -38,14 +38,13 @@ class ParserTests {
         assertTrue(result.containsKey("proxy"));
         assertTrue(result.containsKey("follow"));
         assertEquals("hexlet.io", result.get("host"));
-        assertEquals(timeout, result.get("timeout"));
+        assertEquals(TIMEOUT_1, result.get("timeout"));
         assertEquals("123.234.53.22", result.get("proxy"));
         assertEquals(false, result.get("follow"));
     }
 
     @Test
     void testParseFileValidYmlFile() throws IOException {
-        int timeout = 20;
         String filePath = "src/test/resources/yml1.yml";
         Map<String, Object> result = Parser.parseFile(filePath);
 
@@ -53,7 +52,7 @@ class ParserTests {
         assertTrue(result.containsKey("timeout"));
         assertTrue(result.containsKey("verbose"));
         assertTrue(result.containsKey("host"));
-        assertEquals(timeout, result.get("timeout"));
+        assertEquals(TIMEOUT_2, result.get("timeout"));
         assertEquals(true, result.get("verbose"));
         assertEquals("hexlet.io", result.get("host"));
     }
